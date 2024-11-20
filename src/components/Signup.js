4fa884c392abd82e3f5724fda83fda9df6fa01ad
Rajesh Reddy <rajeshreddy@Rajeshs-MacBook-Air-2.local>
2024-11-19 23:41:47 -0500

    added form in singup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -19,6 +19,44 @@ const SignupPage = () => {
           <img src="/mens.jpeg" alt="Men's Fashion" className="fashion-image" />
           <img src="/womens.jpeg" alt="Women's Fashion" className="fashion-image" />
         </div>
+
+        <div className="signup-form">
+          <h2>Sign up</h2>
+          <form>
+            <div className="form-group">
+              <label>Name:</label>
+              <input type="text" name="name" placeholder="Enter your name" required />
+            </div>
+            <div className="form-group">
+              <label>Email:</label>
+              <input type="email" name="email" placeholder="Enter your email" required />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                required
+              />
+            </div>
+            <div className="form-group gender-group">
+              <label>Gender:</label>
+              <label>
+                <input type="radio" name="gender" value="male" required /> Male
+              </label>
+              <label>
+                <input type="radio" name="gender" value="female" required /> Female
+              </label>
+            </div>
+            <button type="submit" className="register-button">
+              Register
+            </button>
+          </form>
+          <p className="signin-link">
+            Already have an account? <a href="#signin">Sign in</a>
+          </p>
+        </div>
       </div>
     </div>
   );
