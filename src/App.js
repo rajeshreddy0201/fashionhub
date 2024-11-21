a698088446fd324932dc4b56b14a812a90fd5bc7
Rajesh Reddy <rajeshreddy@Rajeshs-MacBook-Air-2.local>
2024-11-21 15:13:35 -0500

    added css for nav links in signup and signin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Signup from "./components/Signup";
-import Signin from "./components/Signin";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
 
 const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-        </Routes>
+      <Routes>
+      <Route path="/" element={<SignIn />} /> 
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} /> 
+      </Routes>
     </Router>
   );
 };
